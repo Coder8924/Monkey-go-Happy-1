1d2123f42632c3d3ac2e717acc7e1c1c9866c066
Coder8924 <73396937+Coder8924@users.noreply.github.com>
2020-11-05 19:18:04 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -6,7 +6,7 @@ var score=0;
 function preload(){
   
   
-  monkey_running =            loadAnimation("sprite_0.png","sprite_1.png","sprite_2.png","sprite_3.png","sprite_4.png","sprite_5.png","sprite_6.png","sprite_7.png","sprite_8.png")
+  monkey_running = loadAnimation("sprite_0.png","sprite_1.png","sprite_2.png","sprite_3.png","sprite_4.png","sprite_5.png","sprite_6.png","sprite_7.png","sprite_8.png")
   
   bananaImage = loadImage("banana.png");
   obstacleImage = loadImage("obstacle.png");
@@ -21,10 +21,10 @@ function setup() {
   monkey = createSprite(80,315,20,20);
   monkey.addAnimation("moving", monkey_running);
   monkey.scale = 0.1;
-  // monkey.debug = true;
+ //monkey.debug = true;
   
   ground = createSprite(400,350,900,10);
-  ground.velocityX = -4;
+  ground.velocityX = -8;
   ground.x = ground.width/2;
   console.log(ground.x);
   
@@ -39,7 +39,7 @@ function draw() {
 background(225);
   
   if(foodGroup.isTouching(monkey)) {
-    score = score+1;
+    score = Math.round(frameCount/frameRate())
   }
   
   stroke("black");
@@ -66,7 +66,7 @@ background(225);
   if(keyDown("space")&& monkey.y >= 200 ) {
     monkey.velocityY = -12;
   }
-  monkey.velocityY = monkey.velocityY + 0.8;
+  monkey.velocityY = monkey.velocityY + 1;
   
   monkey.collide(ground);
   
@@ -84,10 +84,13 @@ function spawnFood() {
     banana.y = Math.round(random(250,120));
     banana.addImage(bananaImage);
     banana.scale = 0.1;
-    banana.velocityX = -4;
+    banana.velocityX = -8;
     
      //assign lifetime to the variable
     banana.lifetime = 200;
+    
+    banana.setCollider("circle", 50,50);
+//  banana.debug=true;
     
     //adjust the depth
     banana.depth = monkey.depth;
@@ -106,17 +109,16 @@ function spawnObstacle() {
     obstacle.y = Math.round(random(326,325));
     obstacle.addImage(obstacleImage);
     obstacle.scale = 0.1;
-    obstacle.velocityX = -4;
+    obstacle.velocityX = -8;
     
      //assign lifetime to the variable
     obstacle.lifetime = 200;
-  
+    
+    obstacle.setCollider("circle", 50,50);
+//  obstacle.debug=true;
+    
     //add each banana to the group
     obstacleGroup.add(obstacle);
   }
 }
-
-
-
-
 
